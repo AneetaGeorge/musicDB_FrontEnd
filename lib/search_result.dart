6db287bbb5ea5fd620d8ecaadf4c44bbec501511
Aneeta George <aneetageorge34@gmail.com>
2023-09-27 14:42:38 -0700

    Added artist album details in the search result

--- lib/search_result.dart
+++ lib/search_result.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:music_db/entities/artist.dart';
+import 'package:music_db/models/artist.dart';
+import 'package:music_db/models/release_group.dart';
 import 'package:music_db/repository/artist_repo.dart';
+import 'package:music_db/widgets/album_widget.dart';
+import 'package:music_db/widgets/artist_widget.dart';
+import 'package:music_db/widgets/progress_widget.dart';
 
 class SearchResult extends StatelessWidget {
   const SearchResult({super.key});
@@ -14,30 +18,43 @@ class SearchResult extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-          child : FutureBuilder<Artist>(
-              future: artistRepo.getArtistDetails(keyword),
-              builder: (BuildContext context, AsyncSnapshot<Artist> snapshot) {
-                if (snapshot.hasData) {
-                  return Column(
-                    children: [
-                      Text(snapshot.data!.name),
-                      Text(snapshot.data!.sortName),
-                      Text(snapshot.data!.type!),
-                      Text(snapshot.data!.country!)
-                    ],
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('${snapshot.error}');
-                }
-                else {
-                  return const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Center(
-                        child: CircularProgressIndicator()
-                    ),
-                  );
-                }
-              }
+          child : Column(
+            children: [
+              FutureBuilder<Artist>(
+                  future: artistRepo.getArtistDetails(keyword),
+                  builder: (BuildContext context, AsyncSnapshot<Artist> snapshot) {
+                    if (snapshot.hasData) {
+                      return ArtistWidget(artist: snapshot.data!);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+                    else {
+                      return const ProgressWidget();
+                    }
+                  }
+              ),
+              FutureBuilder<List<ReleaseGroup>>(
+                            future: artistRepo.getArtistAlbums(keyword),
+                            builder: (BuildContext context, AsyncSnapshot<List<ReleaseGroup>> snapshot) {
+                              if (snapshot.hasData) {
+                                return Expanded(
+                                  child: ListView.builder(
+                                      itemCount: snapshot.data!.length,
+                                      itemBuilder: (BuildContext context, int index) {
+                                        return AlbumWidget(album: snapshot.data![index]);
+                                      }
+                                  ),
+                                );
+                              }
+                              else if (snapshot.hasError) {
+                                return Text('${snapshot.error}');
+                              }
+                              else {
+                                return const ProgressWidget();
+                              }
+                            }
+              )
+            ],
           ),
         )
       )
