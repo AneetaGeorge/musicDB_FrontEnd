8c42be937cea7b4ddeaa99c8804bc033e812af79
Aneeta George <aneetageorge34@gmail.com>
2023-10-12 14:55:22 -0700

    Added functions to get releases of a release group and tracks in a release group

--- lib/repository/artist_repo.dart
+++ lib/repository/artist_repo.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:music_db/models/artist.dart';
 import 'package:music_db/models/recording.dart';
+import 'package:music_db/models/release.dart';
 import 'package:music_db/models/release_group.dart';
 
 class ArtistRepository {
   static Future<Artist> getArtistDetails(String keyword) async {
-    Uri url = Uri.parse('http://10.0.2.2:8001/api/artist/$keyword');
+    Uri uri = Uri.parse('http://10.0.2.2:8001/api/artist/$keyword');
     try {
-      final response = await http.get(url);
+      final response = await http.get(uri);
 
       if (response.statusCode == 200) {
         final dynamic decodedList = jsonDecode(response.body);
@@ -28,9 +29,9 @@ class ArtistRepository {
   }
 
   static Future<List<ReleaseGroup>> getArtistAlbums(String keyword) async {
-    Uri url = Uri.parse('http://10.0.2.2:8001/api/artist/$keyword/albums');
+    Uri uri = Uri.parse('http://10.0.2.2:8001/api/artist/$keyword/albums');
     try {
-      final response = await http.get(url);
+      final response = await http.get(uri);
 
       if (response.statusCode == 200) {
         final List<dynamic> decodedList = jsonDecode(response.body) as List;
@@ -54,9 +55,9 @@ class ArtistRepository {
   }
 
   static Future<List<Recording>> getAlbumTracks(ReleaseGroup album) async {
-    Uri url = Uri.parse('http://10.0.2.2:8001/api/album/${album.title}/songs');
+    Uri uri = Uri.parse('http://10.0.2.2:8001/api/album/${album.title}/songs');
    try {
-      final response = await http.get(url);
+      final response = await http.get(uri);
 
       if (response.statusCode == 200) {
         final List<dynamic> decodedJson = jsonDecode(response.body) as List;
@@ -75,5 +76,49 @@ class ArtistRepository {
     catch (e) {
      rethrow;
    }
+  }
+
+  static Future getAlbumReleases(ReleaseGroup album) async {
+    Uri uri = Uri.parse('http://10.0.2.2:8001/api/album/${album.id}/releases');
+    try {
+      final response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> decodeJson = jsonDecode(response.body) as List;
+        List<Release> releases = decodeJson.map((e) => Release.fromJson(e)).toList();
+        return releases;
+      }
+      else if (response.statusCode == 404) {
+        throw 'Sorry, we could not find data for ${album.title}';
+      }
+      else {
+        throw 'Request failed with status code ${response.statusCode}';
+      }
+    }
+    catch (e) {
+      rethrow;
+    }
+  }
+
+  static Future getReleaseTracks(Release release) async {
+    Uri uri = Uri.parse('http://10.0.2.2:8001/api/release/${release.id}/songs');
+    try {
+      final response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> decodedJson = jsonDecode(response.body) as List;
+        List<Recording> recordings = decodedJson.map((e) => Recording.fromJson(e)).toList();
+        return recordings;
+      }
+      else if (response.statusCode == 404) {
+        throw 'Sorry, we could not find data for ${release.title}';
+      }
+      else {
+        throw 'Request failed with status code ${response.statusCode}';
+      }
+    }
+    catch (e) {
+      rethrow;
+    }
   }
 }
