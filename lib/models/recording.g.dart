0423500056573b7fc8dcbc8726ca512129a4934a
Aneeta George <aneetageorge34@gmail.com>
2023-10-12 14:59:44 -0700

    Removed releases from recording

--- lib/models/recording.g.dart
+++ lib/models/recording.g.dart
@@ -11,9 +11,6 @@ Recording _$RecordingFromJson(Map<String, dynamic> json) => Recording(
       title: json['title'] as String,
       length: json['length'] as int?,
       firstReleaseDate: json['first-release-date'] as String?,
-      // releases: (json['releases'] as List<dynamic>)
-      //     .map((e) => Release.fromJson(e as Map<String, dynamic>))
-      //     .toList(),
     );
 
 Map<String, dynamic> _$RecordingToJson(Recording instance) => <String, dynamic>{
@@ -21,5 +18,4 @@ Map<String, dynamic> _$RecordingToJson(Recording instance) => <String, dynamic>{
       'title': instance.title,
       'length': instance.length,
       'first-release-date': instance.firstReleaseDate,
-      // 'releases': instance.releases.map((e) => e.toJson()).toList(),
     };
