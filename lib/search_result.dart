28774121bfd53ad42211c2477283f1e6b1dec896
Aneeta George <aneetageorge34@gmail.com>
2023-10-23 15:39:49 -0700

    Display artist name in AppBar

--- lib/search_result.dart
+++ lib/search_result.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:music_db/models/artist.dart';
+// import 'package:music_db/models/artist.dart';
 import 'package:music_db/models/release_group.dart';
 import 'package:music_db/repository/artist_repo.dart';
 import 'package:music_db/widgets/album_widget.dart';
-import 'package:music_db/widgets/artist_widget.dart';
+// import 'package:music_db/widgets/artist_widget.dart';
 import 'package:music_db/widgets/progress_widget.dart';
-// import 'package:async/async.dart';
 
 class SearchResult extends StatefulWidget {
   final String keyword;
@@ -28,23 +27,26 @@ class _SearchResultState extends State<SearchResult> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text(widget.keyword),
+          centerTitle: true,
+        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            FutureBuilder<Artist>(
-                future: ArtistRepository.getArtistDetails(widget.keyword),
-                builder: (BuildContext context, AsyncSnapshot<Artist> snapshot) {
-                  if (snapshot.hasData) {
-                    return ArtistWidget(artist: snapshot.data!);
-                  } else if (snapshot.hasError) {
-                    return Text('${snapshot.error}');
-                  }
-                  else {
-                    return const ProgressWidget();
-                  }
-                }
-            ),
+            // FutureBuilder<Artist>(
+            //     future: ArtistRepository.getArtistDetails(widget.keyword),
+            //     builder: (BuildContext context, AsyncSnapshot<Artist> snapshot) {
+            //       if (snapshot.hasData) {
+            //         return ArtistWidget(artist: snapshot.data!);
+            //       } else if (snapshot.hasError) {
+            //         return Text('${snapshot.error}');
+            //       }
+            //       else {
+            //         return const ProgressWidget();
+            //       }
+            //     }
+            // ),
             FutureBuilder(
                           future: _artistRepoFuture,
                           builder: (BuildContext context, AsyncSnapshot snapshot) {
