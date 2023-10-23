9f6d483f148d8e0b8b6b5ecb292dd84504d099b7
Aneeta George <aneetageorge34@gmail.com>
2023-10-23 15:40:21 -0700

    Render image using CoverArtWidget

--- lib/widgets/track_widget.dart
+++ lib/widgets/track_widget.dart
@@ -1,13 +1,14 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:music_db/models/recording.dart';
 import 'package:duration/duration.dart';
+import 'package:music_db/models/release.dart';
 import 'package:music_db/repository/cover_art_repo.dart';
-import 'package:music_db/widgets/progress_widget.dart';
+import 'package:music_db/widgets/cover_art_widget.dart';
 
 class TrackWidget extends StatefulWidget {
   final Recording track;
-  const TrackWidget({super.key, required this.track});
+  final Release release;
+  const TrackWidget({super.key, required this.track, required this.release});
 
   @override
   State<TrackWidget> createState() => _TrackWidgetState();
@@ -19,7 +20,7 @@ class _TrackWidgetState extends State<TrackWidget> {
   @override
   void initState() {
     super.initState();
-    trackImgFuture = CoverArtRepo.getReleaseCoverArt(widget.track.id);
+    trackImgFuture = CoverArtRepo.getReleaseCoverArt(widget.release.id);
   }
   
   @override
@@ -30,37 +31,7 @@ class _TrackWidgetState extends State<TrackWidget> {
         ),
         child: Row(
           children: [
-            Container(
-              height: 100,
-              width: 100,
-              padding: const EdgeInsets.only(right: 5),
-              child: FutureBuilder(
-                  future: trackImgFuture,
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.hasData) {
-                      return CachedNetworkImage(
-                        imageUrl: snapshot.data![0].image,
-                        // placeholder: (context, url) => CircularProgressIndicator(),
-                        errorWidget: (context, url, error) => const Icon(Icons.error),
-                        height: 100,
-                        width: 100,
-                      );
-                    }
-                    else if (snapshot.hasError) {
-                      return CachedNetworkImage(
-                        imageUrl: 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg',
-                        // placeholder: (context, url) => CircularProgressIndicator(),
-                        errorWidget: (context, url, error) => const Icon(Icons.error),
-                        height: 100,
-                        width: 100,
-                      );
-                    }
-                    else {
-                      return const ProgressWidget();
-                    }
-                  }
-              ),
-            ),
+            CoverArtWidget(coverFuture: trackImgFuture, height: 80, width: 80,),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
